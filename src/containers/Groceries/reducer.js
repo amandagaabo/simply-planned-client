c09c0bad018e52f0356933f9746fe1975fcdb9f9
souldzin <souldzin@souldzin.com>
2017-12-15 08:19:46 -0600

    refactored Groceries reducer to use switch

--- src/containers/Groceries/reducer.js
+++ src/containers/Groceries/reducer.js
@@ -20,94 +20,85 @@ const initialState = {
 };
 
 export default function (state=initialState, action) {
-  // FETCH GROCERIES
-  if (action.type === FETCH_GROCERIES_REQUEST) {
-    return {
-      ...state,
-      loading: true
-    }
-  } else if (action.type === FETCH_GROCERIES_SUCCESS) {
-    return {
-      ...state,
-      loading: false,
-      groceries: action.groceries || []
-    }
-  } else if (action.type === FETCH_GROCERIES_ERROR) {
-    console.log('fetch groceries action error', action.error)
-    return {
-      ...state,
-      loading: false,
-      error: action.error
-    }
+  switch(action.type) {
+    case FETCH_GROCERIES_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_GROCERIES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        groceries: action.groceries || []
+      }
+    case FETCH_GROCERIES_ERROR:
+      console.log('fetch groceries action error', action.error)
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    case ADD_GROCERY_ITEM_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case ADD_GROCERY_ITEM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        groceries: [...state.groceries, {
+          id: action.item.id,
+          name: action.item.name,
+          checked: action.item.checked
+        }]
+      }
+    case ADD_GROCERY_ITEM_ERROR:
+      console.log('add grocery action error', action.error)
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    case TOGGLE_CHECKED_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case TOGGLE_CHECKED_SUCCESS:
+      return {
+        ...state,
+        groceries: state.groceries.map( item => item.id === action.item.id
+          ? {...item, checked: action.item.checked}
+          : item
+        )
+      }
+    case TOGGLE_CHECKED_ERROR:
+      console.log('toggle action error', action.error)
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    case REMOVE_CHECKED_ITEMS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case REMOVE_CHECKED_ITEMS_SUCCESS:
+      return {
+        ...state,
+        groceries: state.groceries.filter( item => item.checked === false )
+      }
+    case REMOVE_CHECKED_ITEMS_ERROR:
+      console.log('remove checked items error', action.error)
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    default:
+      return state;
   }
-
-  // ADD GROCERY
-  else if (action.type === ADD_GROCERY_ITEM_REQUEST) {
-    return {
-      ...state,
-      loading: true
-    }
-  } else if (action.type === ADD_GROCERY_ITEM_SUCCESS) {
-    return {
-      ...state,
-      loading: false,
-      groceries: [...state.groceries, {
-        id: action.item.id,
-        name: action.item.name,
-        checked: action.item.checked
-      }]
-    }
-  } else if (action.type === ADD_GROCERY_ITEM_ERROR) {
-    console.log('add grocery action error', action.error)
-    return {
-      ...state,
-      loading: false,
-      error: action.error
-    }
-  }
-
-  // TOGGLE GROCERY ITEMS
-  else if (action.type === TOGGLE_CHECKED_REQUEST) {
-    return {
-      ...state,
-      loading: true
-    }
-  } else if (action.type === TOGGLE_CHECKED_SUCCESS) {
-    return {
-      ...state,
-      groceries: state.groceries.map( item => item.id === action.item.id
-        ? {...item, checked: action.item.checked}
-        : item
-      )
-    }
-  } else if (action.type === TOGGLE_CHECKED_ERROR) {
-    console.log('toggle action error', action.error)
-    return {
-      ...state,
-      loading: false,
-      error: action.error
-    }
-  }
-
-  // REMOVE CHECKED GROCERY ITEMS
-  else if (action.type === REMOVE_CHECKED_ITEMS_REQUEST) {
-    return {
-      ...state,
-      loading: true
-    }
-  } else if (action.type === REMOVE_CHECKED_ITEMS_SUCCESS) {
-    return {
-      ...state,
-      groceries: state.groceries.filter( item => item.checked === false )
-    }
-  } else if (action.type === REMOVE_CHECKED_ITEMS_ERROR) {
-    console.log('remove checked items error', action.error)
-    return {
-      ...state,
-      loading: false,
-      error: action.error
-    }
-  }
-
-  return state;
 };
