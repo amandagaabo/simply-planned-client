1a0572d30f7b317878baa7ce6457bd36d87835fb
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-12-19 12:29:16 -0700

    removed drop down delete menu for gorcery list

--- src/containers/Groceries/components/layout.js
+++ src/containers/Groceries/components/layout.js
@@ -6,13 +6,16 @@ import './layout.css';
 
 export default function Layout(props) {
   function onItemClick(e) {
-    const id= e.target.id;
+    const id = e.target.id;
     const checked = e.target.className === 'checked';
     props.onToggle(props.authToken, id, checked);
   };
 
   function onRemoveButtonClick() {
-    props.onRemoveItems(props.authToken)
+    const confirmDelete = window.confirm('Delete all crossed off items?');
+    if(confirmDelete) {
+      props.onRemoveItems(props.authToken)
+    }
   }
 
   const itemList = props.groceries.map( (item) => {
@@ -27,14 +30,9 @@ export default function Layout(props) {
 
   return (
     <div className="row">
-      <ul className="edit-menu">
-        <li className="dropdown">
-          <span className="dropbtn"><Icon name="trash-o" /></span>
-          <div className="dropdown-content">
-            <span className="remove-items" onClick={onRemoveButtonClick}>Delete Crossed Items</span>
-          </div>
-        </li>
-      </ul>
+      <div className="remove-items">
+        <span onClick={onRemoveButtonClick} ><Icon name="trash-o" /></span>
+      </div>
 
       <h2 className="list-header">Grocery List</h2>
 
